642a39693b88e4c7ab78458d3d42feba12aff73f
Victor Walendorff <victor.s.walendorff@gmail.com>
2025-10-15 15:09:40 -0300

    Commit inicial

---
+++ src/main/java/com/mycompany/atividadethreads_sistemasdistribuidos/AtividadeThreads_SistemasDistribuidos.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.atividadethreads_sistemasdistribuidos;
+
+/**
+ *
+ * @author Victor Walendorff
+ */
+public class AtividadeThreads_SistemasDistribuidos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
